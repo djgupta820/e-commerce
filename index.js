3b20bd5c653d2ea229d509aee3aa16294b0f4e66
Dheeraj Gupta <djgupta820@gmail.com>
2023-06-21 11:08:30 +0530

    flash messages feature added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const ejsMate = require('ejs-mate')
 const path = require('path')
+const flash = require('connect-flash')
+const session = require('express-session')
 const methodOverride = require("method-override")
 
 // using all routers
@@ -8,12 +10,27 @@ const productRoutes = require('./routes/productRoutes')
 const reviewRoutes = require('./routes/reviewRouter')
 
 const app = express()
+
+const sessionConfig = {
+    secret: 'weneedgoodsecret',
+    resave: false,
+    saveUninitialized: true
+}
+
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(session(sessionConfig))
+app.use(flash())
+
+app.use((req,res,next)=>{
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
+})
 
 // using product routes
 app.use(productRoutes)
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,8 +4,8 @@ const Product = require('../models/Product')
 
 // get all products
 router.get('/products', async (req,res)=>{
-    const products = await Product.find({}) 
-    res.render('products/index', {products})
+    const products = await Product.find({})
+    res.render('products/index', {products, message: req.flash('message')})
 })
 
 // get form to create new product
@@ -16,8 +16,14 @@ router.get('/products/new', (req,res)=>{
 // create new product
 router.post('/products/new', async (req,res)=>{
     const {name, img, price, desc} = req.body
-    await Product.create({name, img, price, desc})
-    res.redirect('/products')
+    try{
+        await Product.create({name, img, price, desc})
+        req.flash('success', 'Product added successfully! ')
+        res.redirect('/products')
+    }catch{
+        req.flash('error', 'Product added Unsuccessfully! ')
+        res.redirect('/products')
+    }
 })
 
 // show product
@@ -39,13 +45,15 @@ router.patch('/products/:productId', async (req,res)=>{
     const {productId} = req.params
     const {name, img, price, desc} = req.body
     await Product.findByIdAndUpdate(productId, {name, img, price, desc})
-    res.redirect('/products')
+    req.flash('success', 'Product updated successfully!')
+    res.redirect(`/products/${productId}`)
 })
 
 // delete product
 router.delete('/products/:productId', async (req,res)=>{
     const {productId} = req.params
     await Product.findByIdAndDelete(productId)
+    req.flash('success', 'Product deleted successfully!')
     res.redirect('/products')
 })
 
